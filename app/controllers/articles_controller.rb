c875d160db7704e8da7d7a50f1e33173c2799498
Manami Young <manamiyoung@manaminombp.lan>
2023-02-05 14:31:01 +0900

    added new and create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,24 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  # def article_params
+  #   params.require(:article).permit(:title, :description)
+  # end
 end
