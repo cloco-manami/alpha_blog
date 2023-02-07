e9df4c1510dbe2def881864aefdbc5a57875f26e
Manami Young <manamiyoung@ManaminoMacBook-Pro.local>
2023-02-07 16:56:40 +0900

    added edit template and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,15 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was successfully updated."
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
