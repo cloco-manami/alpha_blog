b8eb4b19da600a36e659dd82666f4d678efa7d56
Manami Young <manamiyoung@ManaminoMacBook-Pro.local>
2023-02-07 16:44:47 +0900

    added users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the Alpha Blog, #{@user.username} have successfully signed up."
+      redirect_to articles_path
+    else
+      render 'new'
+    end
   end
 
 
